89447fd7411b2cd8d4d58cdb8487ef6c705bf626
rafaelchs <rafaelchevez4@gmail.com>
2019-09-22 15:52:40 -0600

    ventana crear servicios agregada

---
+++ app/src/main/java/com/example/appglobation/CrearServiciosActivity.java
@@ -0,0 +1,14 @@
+package com.example.appglobation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearServiciosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_servicios);
+    }
+}
