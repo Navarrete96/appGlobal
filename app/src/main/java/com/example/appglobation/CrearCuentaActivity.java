8f242645e39cb6bc844d483d62809136bec3c250
rafael.chevez <rafaelchevez4@gmail.com>
2019-10-15 16:35:03 -0600

    añadido las dependencia de firebase auth, iconos y validacion del usuario

--- app/src/main/java/com/example/appglobation/CrearCuentaActivity.java
+++ app/src/main/java/com/example/appglobation/CrearCuentaActivity.java
@@ -1,21 +1,33 @@
 package com.example.appglobation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CrearCuentaActivity extends AppCompatActivity {
 
     private Button btnCrearCuenta, btnVolver;
+    private EditText editCorreo, editPassword;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_cuenta);
         init();
+        firebaseAuth = FirebaseAuth.getInstance();
 
         btnVolver.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,9 +41,8 @@ public class CrearCuentaActivity extends AppCompatActivity {
         btnCrearCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                MainActivity mainActivity = new MainActivity();
-                Intent intent = new Intent(CrearCuentaActivity.this,mainActivity.getClass());
-                startActivity(intent);
+                createAccount(editCorreo.getText().toString(),editPassword.getText().toString());
+
             }
         });
     }
@@ -39,5 +50,25 @@ public class CrearCuentaActivity extends AppCompatActivity {
     public void init(){
         btnCrearCuenta = findViewById(R.id.crearCuenta);
         btnVolver = findViewById(R.id.crearVolver);
+        editCorreo = findViewById(R.id.editTextCorreo);
+        editPassword = findViewById(R.id.editTextPassword);
+    }
+
+    private void createAccount(String email, String pass){
+        firebaseAuth.createUserWithEmailAndPassword(email,pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            MainActivity mainActivity = new MainActivity();
+                            Intent intent = new Intent(CrearCuentaActivity.this,mainActivity.getClass());
+                            startActivity(intent);
+                        }else{
+
+
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/appglobation/LoginFragment.java
+++ app/src/main/java/com/example/appglobation/LoginFragment.java
@@ -1,15 +1,28 @@
 package com.example.appglobation;
 
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 
 /**
@@ -18,9 +31,10 @@ import android.widget.Button;
 public class LoginFragment extends Fragment {
 
     private Button btnLogin, btnCrear;
+    private EditText editTextEmail, editTextPassword;
+    private FirebaseAuth auth;
 
     public LoginFragment() {
-        // Required empty public constructor
     }
 
 
@@ -28,14 +42,15 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
         init(view);
+        auth = FirebaseAuth.getInstance();
 
         btnCrear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 CrearCuentaActivity cuentaActivity = new CrearCuentaActivity();
-                Intent intent = new Intent(getContext(),cuentaActivity.getClass());
+                Intent intent = new Intent(getContext(), cuentaActivity.getClass());
                 startActivity(intent);
             }
         });
@@ -43,17 +58,51 @@ public class LoginFragment extends Fragment {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                HomeActivity homeActivity = new HomeActivity();
-                Intent intent = new Intent(getContext(),homeActivity.getClass());
-                startActivity(intent);
+
+                if (editTextEmail.getText().toString().isEmpty() || editTextPassword.getText().toString().isEmpty()) {
+                    Toast.makeText(getContext(), "Los campos estan vacios", Toast.LENGTH_LONG).show();
+                } else {
+                    login(editTextEmail.getText().toString(), editTextPassword.getText().toString());
+                }
+
             }
         });
         return view;
     }
 
-    private void init(View v){
+    private void init(View v) {
         btnCrear = v.findViewById(R.id.btnCrear);
         btnLogin = v.findViewById(R.id.btnLogin);
+        editTextEmail = v.findViewById(R.id.editEmail);
+        editTextPassword = v.findViewById(R.id.editPassword);
+    }
+
+    private void login(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            new SweetAlertDialog(getContext(), SweetAlertDialog.SUCCESS_TYPE)
+                                    .setContentText("Bienvenido!")
+                                    .show();
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    HomeActivity homeActivity = new HomeActivity();
+                                    Intent intent = new Intent(getContext(), homeActivity.getClass());
+                                    startActivity(intent);
+                                }
+                            },3000);
+
+                        } else {
+                            new SweetAlertDialog(getContext(), SweetAlertDialog.ERROR_TYPE)
+                                    .setTitleText("Oops...")
+                                    .setContentText("El correo o contraseña son incorrectos")
+                                    .show();
+                        }
+                    }
+                });
     }
 
 }
