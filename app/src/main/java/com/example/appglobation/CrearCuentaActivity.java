4dccc81c20f92791eb549dc886f03fa61339cbad
rafael.chevez <rafaelchevez4@gmail.com>
2019-10-23 16:49:15 -0600

    añadido firebase con crear servicios

--- app/src/main/java/com/example/appglobation/CrearCuentaActivity.java
+++ app/src/main/java/com/example/appglobation/CrearCuentaActivity.java
@@ -3,12 +3,14 @@ package com.example.appglobation;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,14 +18,21 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class CrearCuentaActivity extends AppCompatActivity {
 
     private Button btnCrearCuenta, btnVolver;
-    private EditText editCorreo, editPassword;
+    private EditText editCorreo, editPassword, editTextFechaNacimiento,editTextNombre,editTextPrimerApellido, editTextSegundoApellido;
     private FirebaseAuth firebaseAuth;
+    private static final String ZERO = "0";
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,7 @@ public class CrearCuentaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_crear_cuenta);
         init();
         firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference("User");
 
         btnVolver.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +58,13 @@ public class CrearCuentaActivity extends AppCompatActivity {
 
             }
         });
+
+        editTextFechaNacimiento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCalendar();
+            }
+        });
     }
 
     public void init(){
@@ -55,6 +72,38 @@ public class CrearCuentaActivity extends AppCompatActivity {
         btnVolver = findViewById(R.id.crearVolver);
         editCorreo = findViewById(R.id.editTextCorreo);
         editPassword = findViewById(R.id.editTextPassword);
+        editTextFechaNacimiento = findViewById(R.id.idFechaNacimiento);
+        editTextNombre = findViewById(R.id.editTextNombre);
+        editTextPrimerApellido = findViewById(R.id.editTextPrimerApellido);
+        editTextSegundoApellido = findViewById(R.id.editTextSegundoApellido);
+    }
+
+    public void showCalendar() {
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = calendar.get(Calendar.MONTH);
+        int currentYear = calendar.get(Calendar.YEAR);
+        calendar.set(1955,0,1);
+        int year = currentYear - 18;
+
+
+        DatePickerDialog date = new DatePickerDialog(this,R.style.DialogTheme,new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                int currentMonth = month + 1;
+                String dayFormat = (dayOfMonth < 10) ? ZERO + dayOfMonth : String.valueOf(dayOfMonth);
+                String monthFormat = (month < 9) ? ZERO + currentMonth : String.valueOf(currentMonth);
+                editTextFechaNacimiento.setText(year + "-" + monthFormat + "-" + dayFormat);
+            }
+        },year,month,day);
+
+        Calendar maxCalendar = Calendar.getInstance();
+        maxCalendar.set(Calendar.MONTH,month);
+        maxCalendar.set(Calendar.DAY_OF_MONTH,day);
+        maxCalendar.set(Calendar.YEAR,year);
+        date.getDatePicker().setMinDate(calendar.getTimeInMillis());
+        date.getDatePicker().setMaxDate(maxCalendar.getTimeInMillis());
+        date.show();
     }
 
     private void createAccount(String email, String pass){
@@ -64,6 +113,12 @@ public class CrearCuentaActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
                         if(task.isSuccessful()){
+                            DatabaseReference reference = databaseReference.child(firebaseAuth.getCurrentUser().getUid());
+                            reference.child("Nombre").setValue(editTextNombre.getText().toString());
+                            reference.child("PrimerApellido").setValue(editTextPrimerApellido.getText().toString());
+                            reference.child("SegundoApellido").setValue(editTextSegundoApellido.getText().toString());
+                            reference.child("FechaNacimiento").setValue(editTextFechaNacimiento.getText().toString());
+
                             new SweetAlertDialog(CrearCuentaActivity.this, SweetAlertDialog.SUCCESS_TYPE)
                                     .setContentText("Se creo la cuenta.")
                                     .show();
@@ -77,7 +132,7 @@ public class CrearCuentaActivity extends AppCompatActivity {
                             },3000);
 
                         }else{
-
+                            Toast.makeText(getApplicationContext(),"No se pudo crear cuenta en este momento",Toast.LENGTH_LONG).show();
 
                         }
                     }
--- app/src/main/java/com/example/appglobation/CrearServiciosActivity.java
+++ app/src/main/java/com/example/appglobation/CrearServiciosActivity.java
@@ -6,21 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CrearServiciosActivity extends AppCompatActivity {
 
     private Button btnSalir, btnCrearServicio;
+    private EditText editTextName, editTextType, editTextPrice, editTextPhone;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_servicios);
+        databaseReference = FirebaseDatabase.getInstance().getReference("Services");
+        firebaseAuth = FirebaseAuth.getInstance();
         init();
         btnSalir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 HomeActivity homeActivity = new HomeActivity();
-                Intent intent = new Intent(CrearServiciosActivity.this,homeActivity.getClass());
+                Intent intent = new Intent(CrearServiciosActivity.this, homeActivity.getClass());
                 startActivity(intent);
 
             }
@@ -30,11 +42,30 @@ public class CrearServiciosActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                if (editTextName.getText().toString().isEmpty() || editTextType.getText().toString().isEmpty() || editTextPrice.getText().toString().isEmpty()
+                        || editTextPhone.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Por favor, llenar los campos solicitados", Toast.LENGTH_LONG).show();
+                } else {
+                    DatabaseReference reference = databaseReference.child(firebaseAuth.getCurrentUser().getUid());
+                    reference.child("servicio").setValue(editTextName.getText().toString());
+                    reference.child("tipo").setValue(editTextType.getText().toString());
+                    reference.child("precio").setValue(editTextPrice.getText().toString());
+                    reference.child("telefono").setValue(editTextPhone.getText().toString());
+                    HomeActivity homeActivity = new HomeActivity();
+                    Intent intent = new Intent(CrearServiciosActivity.this, homeActivity.getClass());
+                    startActivity(intent);
+                    finish();
+                }
+
             }
         });
     }
 
-    private void init(){
+    private void init() {
+        editTextName = findViewById(R.id.serviceName);
+        editTextType = findViewById(R.id.serviceType);
+        editTextPrice = findViewById(R.id.price);
+        editTextPhone = findViewById(R.id.telephone);
         btnSalir = findViewById(R.id.botonVolver);
         btnCrearServicio = findViewById(R.id.crearServicio);
     }
